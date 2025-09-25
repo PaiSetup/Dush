8301e3abf34aac2071cdf1def08b48ec13c77bbb
MaciejDziuban <dziuban.maciej@gmail.com>
2025-09-26 00:09:46 +0200

    Add pe script

--- projects/bashies/p.sh
+++ projects/bashies/p.sh
@@ -7,3 +7,26 @@ else
 fi
 
 alias pp="p | less"
+
+pe() {
+    pid="$1"
+    if ! [ -f /proc/$pid/environ ]; then
+        pid="$(pgrep -x $1)"
+        res=$?
+        if [ $? != 0 ]; then
+            echo "ERROR: PID of $1 could not be found" >&2
+            return $res
+        fi
+        count="$(echo "$pid" | wc -l)"
+        if [ "$count" != 1 ]; then
+            echo "ERROR: $count $1 processes running. Choose one. PIDs: $(echo "$pid" | tr '\n' ' ')" >&2
+            return 1
+        fi
+        if ! [ -f /proc/$pid/environ ]; then
+            echo "ERROR: PID of $1 could not be found" >&2
+            return 1
+        fi
+    fi
+
+    cat /proc/$pid/environ | tr '\0' '\n'
+}
