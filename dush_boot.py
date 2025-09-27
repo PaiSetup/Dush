a7c557e3cac529c751d8b05e326d9f6972b0b701
MaciejDziuban <dziuban.maciej@gmail.com>
2025-09-27 12:09:36 +0200

    Cleanup dush_boot - reformat, add shebang, make executable

--- dush_boot.py
+++ dush_boot.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 import platform
 from pathlib import Path, PurePosixPath
-
 
 is_windows = platform.system() == "Windows"
 home = Path.home()
 dush_path = Path(__file__).parent
 dush_path_bash_on_windows = ""
 symlink_dir = dush_path / "links"
-pythonpath_separator=";" if is_windows else ":"
+pythonpath_separator = ";" if is_windows else ":"
+
 
 def convert_path_to_bash_compatible(path):
     if is_windows:
@@ -16,6 +18,7 @@ def convert_path_to_bash_compatible(path):
         return PurePosixPath("/", drive, *parts)
     else:
         return path
+
 
 def create_profile_symlinks():
     symlink_dir.mkdir(exist_ok=True, parents=True)
@@ -29,7 +32,7 @@ def create_profile_symlinks():
     ]
     if is_windows:
         profiles += [
-            (home / "Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1", "powershell_profile.ps1", True)
+            (home / "Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1", "powershell_profile.ps1", True),
         ]
 
     result = []
@@ -46,7 +49,8 @@ def create_profile_symlinks():
         symlink_path.symlink_to(real_path)
         result.append(f"{symlink_path} -> {real_path}")
 
-    return '\n'.join(result)
+    return "\n".join(result)
+
 
 def generate_sample_bash_profile():
     dush_path_bash = convert_path_to_bash_compatible(dush_path)
@@ -60,6 +64,7 @@ DUSH_ENABLE_AUTOLOAD=1
 . $DUSH_PATH/projects/bashies/main.sh
 . $DUSH_PATH/projects/yuview/main.sh"""
 
+
 def generate_sample_powershell_profile():
     return f"""\
 $env:PYTHONPATH = "{dush_path}{pythonpath_separator}$env:PYTHONPATH"
@@ -69,6 +74,7 @@ $env:DUSH_ENABLE_AUTOLOAD = $true
 Import-Module $env:DUSH_PATH/projects/bashies/main.ps1
 Import-Module $env:DUSH_PATH/projects/yuview/main.ps1"""
 
+
 def print_section(name, content):
     expected_width = 80
 
@@ -77,17 +83,18 @@ def print_section(name, content):
     if filler_count > 0:
         filler_count_left = filler_count // 2
         filler_count_right = filler_count - filler_count_left
-        header = ('#' * filler_count_left) + name + ('#' * filler_count_right)
+        header = ("#" * filler_count_left) + name + ("#" * filler_count_right)
     else:
         header = name
 
-    footer = '#' * expected_width
+    footer = "#" * expected_width
     print(header)
     print(content)
     print(footer)
     print()
     print()
     print()
+
 
 print_section("Symlinks to profiles", create_profile_symlinks())
 print_section("Example Bash profile", generate_sample_bash_profile())
